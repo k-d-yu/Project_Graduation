2c5a51a6950c6e6614193147e7add26fc243eae1
k-d-yu <dima_kul95@mail.ru>
2023-04-17 20:19:31 +0300

    fourth commit

--- Project/Project/urls.py
+++ Project/Project/urls.py
@@ -20,4 +20,6 @@ from main import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
+    path('about/', views.about, name="about"),
+    path('session/', views.session, name="session"),
 ]
--- Project/main/views.py
+++ Project/main/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import About
 
 
 def index(request):
     return render(request, "main/index.html")
+
+
+def about(request):
+    abouts = About.objects.all()
+    return render(request, "main/about.html", {"abouts": abouts})
+
+
+def session(request):
+    return render(request, "main/session.html")
